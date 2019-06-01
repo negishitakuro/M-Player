33ebd13b3209887bcc8adaabfc0c4638d37c8b50
negishitakuro <t_negi@hotmail.com>
2019-06-01 16:52:13 +0900

    first commit

---
+++ M-PlayerTests/M_PlayerTests.swift
@@ -0,0 +1,34 @@
+//
+//  M_PlayerTests.swift
+//  M-PlayerTests
+//
+//  Created by negishitakuro on 2019/06/01.
+//  Copyright © 2019 negishitakuro. All rights reserved.
+//
+
+import XCTest
+@testable import M_Player
+
+class M_PlayerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
