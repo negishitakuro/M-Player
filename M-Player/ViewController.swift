c005d8e52f67ed8abafeca5a71f0a473c0a78b55
negishitakuro <t_negi@hotmail.com>
2019-06-12 11:34:42 +0900

    曲再生機能追加

--- M-Player/ViewController.swift
+++ M-Player/ViewController.swift
@@ -7,14 +7,70 @@
 //
 
 import UIKit
+//import AVFoundation
+import MediaPlayer
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MPMediaPickerControllerDelegate {
 
+    //MediaPlayerのインスタンスを作成
+    var player:MPMusicPlayerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        player = MPMusicPlayerController.applicationMusicPlayer
+        
+//        let documentDirPath = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+//        print(documentDirPath)
+        
+//        let albumsQuery = MPMediaQuery.albums()
+//        let albums = albumsQuery.collections as! [MPMediaItemCollection]
+//
+//        for album in albums {
+//            let title = album.representativeItem.albumTitle ?? ""  // アルバム名
+//            let artist = album.representativeItem.albumArtist ?? ""  // アーティスト名
+//        }
+        
+    }
+    
+    // メディアアイテムピッカーでアイテムを選択完了したときに呼び出される
+    func mediaPicker(mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        // 選択した曲情報がmediaItemCollectionに入っているので、これをplayerにセット。
+        player.setQueue(with: mediaItemCollection)
+        // 再生開始
+        player.play()
+        // ピッカーを閉じ、破棄する
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //選択がキャンセルされた場合に呼ばれる
+    func mediaPickerDidCancel(mediaPicker: MPMediaPickerController) {
+        // ピッカーを閉じ、破棄する
+        dismiss(animated: true, completion: nil)
     }
 
-
+    @IBAction func pick(_ sender: Any) {
+        // MPMediaPickerControllerのインスタンスを作成
+        let picker = MPMediaPickerController()
+        // ピッカーのデリゲートを設定
+        picker.delegate = self
+        // 複数選択を不可にする。（trueにすると、複数選択できる）
+        picker.allowsPickingMultipleItems = false
+        // ピッカーを表示する
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func pushPlay(_ sender: Any) {
+        player.play()
+    }
+    
+    @IBAction func pushPause(_ sender: Any) {
+        player.pause()
+    }
+    
+    @IBAction func pushStop(_ sender: Any) {
+        player.stop()
+    }
 }
 
